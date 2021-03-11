9b9a1edbbbd68ead152c4a13f65b6dd56fd0c0c9
tahiry-dev <tahirycontact@gmail.com>
2021-03-11 18:10:32 +0300

    fix eslint

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/instant-weather-forecast/'
+    publicPath: '/instant-weather-forecast/',
   },
   module: {
     rules: [
